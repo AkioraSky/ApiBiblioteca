25f139ba0e540fdfd7799bfea27e1ddcd1b32b12
Juan Granada <juanpagv118@gmail.com>
2023-08-30 17:19:46 -0500

    CambioClaseFunciones

---
+++ Biblioteca/Controllers/BibliotecaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+   // [Route("api/[controller]")]
+    [ApiController]
+    public class BibliotecaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("TraerUsuarios")]
+        public string func_sasa()
+        {
+            string name = "";
+            string json = ClaseFunciones.Func_Traer_Usuario(name);
+            return json;
+        }
+    }
+}
--- Biblioteca/Controllers/ClaseFunciones.cs
+++ Biblioteca/Controllers/ClaseFunciones.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using Newtonsoft.Json;
 
 namespace Biblioteca.Controllers
@@ -9,6 +10,25 @@ namespace Biblioteca.Controllers
         public static string cadena = "Server=(local)\\SQLExpress;Database=BD_Biblioteca;User Id=ADSO2501875;Password=12345;";
         public static string exception = "";
 
+        public static string Func_Traer_Usuario(string name)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection cnn = new SqlConnection(cadena);
+                SqlDataAdapter adp = new SqlDataAdapter("PA_ConsultaUsuario", cnn);
+                adp.SelectCommand.CommandType = CommandType.StoredProcedure;
+                adp.SelectCommand.Parameters.AddWithValue("@nomuser", name);
+                adp.Fill(dt);
+                return JsonConvert.SerializeObject(dt);
+
+            }
+            catch (Exception e)
+            {
+                exception = e.Message;
+                return exception;
+            }
+        }
 
     }
 }
