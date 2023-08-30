be2dec7230823b6c0c7e56eef433a2f5c300fc8a
Yasuke_Sky <kh2.0acru@gmail.com>
2023-08-30 16:59:13 -0500

    cadena de conexion

--- Biblioteca/Controllers/ClaseFunciones.cs
+++ Biblioteca/Controllers/ClaseFunciones.cs
@@ -5,5 +5,10 @@ namespace Biblioteca.Controllers
 {
     public class ClaseFunciones
     {
+
+        public static string cadena = "Server=(local)\\SQLExpress;Database=BD_Biblioteca;User Id=ADSO2501875;Password=12345;";
+        public static string exception = "";
+
+
     }
 }
